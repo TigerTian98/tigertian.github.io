e20493b35a83d2a5a0a8a64e997bc2c892400c11
TigerTian98 <42988622+TigerTian98@users.noreply.github.com>
2023-03-25 16:38:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let menuBool = false;
 function menuToggle(){
 	if (menuBool) {
 		document.getElementById('menu').style.display = "none";
-		document.getElementById('menuButton').textContent = "···";
+		document.getElementById('menuButton').textContent = "≡";
 	} else {
 		document.getElementById('menu').style.display = "block";
 		document.getElementById('menuButton').textContent = "×";
