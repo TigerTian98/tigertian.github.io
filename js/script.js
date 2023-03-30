bfe0ca425ddc6de4f5ed1a2039f4da3b6eeea4e3
TigerTian98 <42988622+TigerTian98@users.noreply.github.com>
2023-03-30 03:21:29 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let menuBool = false;
+
+function menuClose() {
+	if (menuBool) {
+		menuOff();
+	}
+}
+
+function menuOff() {
+	document.getElementById('menuButton').textContent = "≡";
+	document.getElementById('menuBg').style.animation = "menuOffBg 0.5s ease 0s 1 normal both";
+	document.getElementById('menuBox').style.animation = "menuOffText 0.5s ease 0s 1 normal both";
+	document.getElementById('menuBtmShadow').style.animation = "menuOffShadow 1s ease 0s 1 normal both";
+	menuBool = false;
+}
+
+function menuOn() {
+	document.getElementById('menuButton').textContent = "×";
+	document.getElementById('menuBg').style.animation = "menuOnBg 1s ease 0s 1 normal both";
+	document.getElementById('menuBox').style.animation = "menuOnText 1.5s ease 0s 1 normal both";
+	document.getElementById('menuBtmShadow').style.animation = "menuOnShadow 1s ease 0s 1 normal both";
+	menuBool = true;
+}
+
+function menuToggle() {
+	if (menuBool) {
+		menuOff();
+	} else {
+		menuOn();
+	}
+}
+
+function titleAppear() {
+	document.getElementById('header').style.animation = "openingTitle 1.2s ease 0s 1 normal both";	
+}
